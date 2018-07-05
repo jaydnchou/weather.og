13838dfa95b34b3e10b9942e1aa43de5076c47fb
Jaydn Chou <jaydn.chou@gmail.com>
2018-07-06 00:01:41 +1000

    Created ForecastWeekly component

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import ForecastWeekly from './ForecastWeekly';
 import backgroundImage from '../images/leaves2.jpg';
 
 const Forecast = () => (
   <div className='container'>
     <div>
       <img className='background' src={backgroundImage} alt='background' />
+      <h1 className='card-img-overlay'>-3&deg;</h1>
     </div>
 
     <div className='carousel slide' data-ride='carousel'>
-
       <ol className='carousel-indicators'>
         <li className='active' data-slide-to='0' />
         <li data-slide-to='1' />
@@ -18,23 +20,8 @@ const Forecast = () => (
         <div className='carousel-item active'>
           <h2>Pretty, but cold as fuck</h2>
         </div>
+
         <div className='container'>
-          <div className='section'>
-            <ul className='weekly-forecast'>
-              <li>Monday</li>
-              <li>Tuesday</li>
-              <li>Wednesday</li>
-              <li>Thursday</li>
-              <li>Friday</li>
-            </ul>
-            <ul className='weekly-forecast'>
-              <li>-1&deg;</li>
-              <li>2&deg;</li>
-              <li>5&deg;</li>
-              <li>0&deg;</li>
-              <li>-1&deg;</li>
-            </ul>
-          </div>
         </div>
       </div>
     </div>
---
+++ src/components/ForecastWeekly.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ForecastWeekly = () => (
+  <div className='section'>
+    <ul className='weekly-forecast'>
+      <li>Monday</li>
+      <li>Tuesday</li>
+      <li>Wednesday</li>
+      <li>Thursday</li>
+      <li>Friday</li>
+    </ul>
+    <ul className='weekly-forecast'>
+      <li>-1&deg;</li>
+      <li>2&deg;</li>
+      <li>5&deg;</li>
+      <li>0&deg;</li>
+      <li>-1&deg;</li>
+    </ul>
+  </div>
+);
+
+export default ForecastWeekly;
