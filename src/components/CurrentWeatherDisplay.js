c5339bee0e22f5e780ebad4b8d1e644c4b983ced
Jaydn Chou <jaydn.chou@gmail.com>
2018-07-07 23:48:57 +1000

    Added user geolocation functionality

--- src/components/CurrentWeatherDisplay.js
+++ src/components/CurrentWeatherDisplay.js
@@ -3,33 +3,41 @@ import PropTypes from 'prop-types';
 
 
 const getUpdateTime = date => {
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padEnd(2, '0');
-    return `${hours}:${minutes}`;
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padEnd(2, '0');
+  return `${hours}:${minutes}`;
 };
 
-const CurrentWeatherDisplay = props => {
-    
-    const { weather } = props;
-    
-    return (
-        <div className="current-weather-display" style={{position: 'relative'}}>
-            <div className="weather-location">{weather.location.name}</div>
-            <div className="weather-min-max-temp">{weather.temperature.maximum}&deg; | {weather.temperature.minimum}&deg;</div>
-            <div className="weather-current">                
-                <span className="weather-temp">{parseInt(weather.temperature.current, 10)} &deg;&nbsp;<sup>c</sup></span>
-            </div>
-            <div className="weather-condition">
-                <span className="weather-description">{weather.condition}</span>
-            </div>            
-            <div className="weather-update">Updated as of {getUpdateTime(weather.date)}</div>
-            <i className="refresh fa fa-refresh fa-3x" onClick={props.onRefresh}></i>
-        </div>
-    );
-};
-CurrentWeatherDisplay.propTypes = {
-    weather: PropTypes.object.isRequired
-};
+// const CurrentWeatherDisplay = props => {
+
+//     const { weather } = props;
+
+//     return (
+//         <div className="current-weather-display" style={{position: 'relative'}}>
+//             <div className="weather-location">{weather.location.name}</div>
+//             <div className="weather-min-max-temp">{weather.temperature.maximum}&deg; | {weather.temperature.minimum}&deg;</div>
+//             <div className="weather-current">                
+//                 <span className="weather-temp">{parseInt(weather.temperature.current, 10)} &deg;&nbsp;<sup>c</sup></span>
+//             </div>
+//             <div className="weather-condition">
+//                 <span className="weather-description">{weather.condition}</span>
+//             </div>            
+//             <div className="weather-update">Updated as of {getUpdateTime(weather.date)}</div>
+//             <i className="refresh fa fa-refresh fa-3x" onClick={props.onRefresh}></i>
+//         </div>
+//     );
+// };
+// CurrentWeatherDisplay.propTypes = {
+//     weather: PropTypes.object.isRequired
+// };
+
+// const CurrentWeatherDisplay = () => {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//       return `lat: ${position.coords.latitude} long: ${position.coords.longitude}`;
+//     });
+//   } else return 'Gotta accept that popup!';
+// };
 
 
-export default CurrentWeatherDisplay;
+// export default CurrentWeatherDisplay;
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -3,11 +3,38 @@ import React from 'react';
 import ForecastWeekly from './ForecastWeekly';
 import backgroundImage from '../images/leaves2.jpg';
 
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+// const success = pos => {
+//   const coords = pos.coords;
+//   console.log(coords);
+//   return `lat: ${coords.latitude} long: ${coords.longitude}`;
+// };
+
+const success = pos => {
+  const coords = pos.coords;
+  return coords.latitude;
+};
+
+console.log(success);
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+const currentForecast = navigator.geolocation.getCurrentPosition(success, error, options);
+
 const Forecast = () => (
   <div className='container'>
     <div>
       <img className='background' src={backgroundImage} alt='background' />
       <h1 className='card-img-overlay'>-3&deg;</h1>
+      <h1 className='card-img-overlay'>{currentForecast}</h1>
     </div>
 
     <div className='carousel slide' data-ride='carousel'>
